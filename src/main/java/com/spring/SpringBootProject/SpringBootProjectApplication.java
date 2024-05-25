e550fe6aae0ec03edd2add7f0117221dc2ffc247
Rahul Bansal <2311rahulbansal@gmail.com>
2024-05-25 14:58:50 +0530

    initial commit

---
+++ src/main/java/com/spring/SpringBootProject/SpringBootProjectApplication.java
@@ -0,0 +1,16 @@
+package com.spring.SpringBootProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBootProjectApplication.class, args);
+
+		System.out.println("server is running on port:8080");
+	}
+
+}
